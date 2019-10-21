146c261bd5d58439091096f1044cc81916b3e7c7
mudrakpatel <mudrakpatel96@gmail.com>
2019-10-21 16:51:32 +0530

    Added Redux and created reducers

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    
+};
+
+const authReducer = (state = initialState, action) => {
+    return state;
+    /**switch(action.type){
+
+    }**/
+}
+
+export default authReducer;
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+
+};
+
+const projectReducer = (state = initialState, action) => {
+    return state;
+    /**switch(action.type){
+
+    }**/
+}
+
+export default projectReducer;
