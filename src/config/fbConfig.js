05ff84d8aee5e7569ce21e14f08f04010451363d
mudrakpatel <mudrakpatel96@gmail.com>
2019-10-22 15:48:05 +0530

    Created Firebase project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'; //Database
+import 'firebase/auth'; //Authentication
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+     apiKey: "AIzaSyBWQO9BdslAnFjp6XnDkBOb83TaO3V6mJE",
+     authDomain: "marioplan-1860c.firebaseapp.com",
+     databaseURL: "https://marioplan-1860c.firebaseio.com",
+     projectId: "marioplan-1860c",
+     storageBucket: "marioplan-1860c.appspot.com",
+     messagingSenderId: "703689457156",
+     appId: "1:703689457156:web:1069307c18e2ea351abf51",
+     measurementId: "G-NQVS716WTL"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.firestore().settings({timestampsInSnapshots: true,});
+ firebase.analytics();
+
+ export default firebase;
