a25ced5f79332ec2bcfdef1c4041baecb5112125
mudrakpatel <mudrakpatel96@gmail.com>
2019-10-26 17:22:18 +0530

    projectCreated cloud function created and deployed firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+//Initialize the app
+admin.initializeApp(functions.config().firebase);
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+const createNotification = (notification => {
+    return admin
+           .firestore()
+           .collection('notifications')
+           .add(notification)
+           .then(document => {
+               console.log("Notification added: ", document);
+           });
+});
+
+//Listen for a project creation event on firestore database
+exports.projectCreated = functions
+    .firestore
+    .document('projects/{projectId}')
+    .onCreate(document => {
+        const project = document.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${
+                        project.authorFirstName 
+                        + " " +
+                        project.authorLastName
+                    }`,
+            time: admin.firestore().FieldValue.serverTimestamp(),
+        };
+        //Return statement to end the function execution
+        return createNotification(notification);
+    });
